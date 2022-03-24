a34685438d07f7e673c127beed87d4d36f89e57c
Yasiarat <98023179+Yasiarat@users.noreply.github.com>
2022-03-24 14:23:43 +0300

    Add files via upload

--- generate num.py
+++ generate num.py
@@ -3,18 +3,18 @@ def generate_numbers(n: int, m: int, prefix=None) -> None:
     Выводит в консоль все перестановки длиной n в системе счисления n
     """
     if m > n:
-        print('Оно так не работает, чувак')
-        return
+        raise ValueError('Оно так не работает, чувак') # выдаём ошибку
 
     if m == 0:
         print(*prefix)
         return
     prefix = prefix or []
     for digit in range(n):
-        if digit not in prefix:
-            prefix.append(digit)
-            generate_numbers(n, m-1, prefix)
-            prefix.pop()
+        if digit in prefix:
+            continue
+        prefix.append(digit)
+        generate_numbers(n, m-1, prefix)
+        prefix.pop()
 
 
-generate_numbers(3, 3)
+generate_numbers(1, 5)
