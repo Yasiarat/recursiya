2c672629cef209c1fb8c4da06cebff4b6963d1ea
Yasiarat <98023179+Yasiarat@users.noreply.github.com>
2022-05-12 20:10:13 +0300

    Update sistem.py

--- sistem.py
+++ sistem.py
@@ -1,11 +1,13 @@
 def domaetodoma():
-    for x in range(10):
+    for x in range(1000):
         r = 216 ** 5 + 6 ** 3 - 1 - x
         s = ''
-        while x:
-            s += str(x % 2)
-            x //= 2
-        print(s.count('1'))
+        while r:
+            s += str(r % 6)
+            r //= 6
+        if s.count('5') == 12:
+            print(x)
+            break
 
 
 domaetodoma()
